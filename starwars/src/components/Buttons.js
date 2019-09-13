83c4cd32339fb346eadc59ef9403da0330a1f177
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-13 13:13:12 +0100

    convert Button styles to styled-components

--- starwars/src/components/Buttons.js
+++ starwars/src/components/Buttons.js
@@ -1,15 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 import "./Buttons.css";
 
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 2rem;
+
+  button {
+    min-width: 10rem;
+    min-height: 48px;
+    padding: 0.7rem;
+    text-transform: uppercase;
+    background: none;
+    border: 1px solid rgba(0, 0, 0, 0.7);
+    border-radius: 2px;
+
+    &:hover {
+      background: rgba(0, 0, 0, 0.7);
+      color: white;
+      cursor: pointer;
+    }
+
+    &:disabled {
+      cursor: not-allowed;
+    }
+  }
+`;
+
 const Buttons = ({ next, previous, action }) => (
-  <div className="Buttons">
+  <ButtonContainer>
     <button onClick={() => action(previous)} disabled={previous === null}>
       Previous
     </button>
     <button onClick={() => action(next)} disabled={next === null}>
       Next
     </button>
-  </div>
+  </ButtonContainer>
 );
 
 export default Buttons;
