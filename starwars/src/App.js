c5f1039cf837e49e0aba05c1a8c04349757f50df
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-13 09:56:54 +0100

    update Character component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [state, setState] = useState({});
+
+  const getData = url => {
+    return axios
+      .get(url, {
+        mode: "no-cors"
+      })
+      .then(({ data }) => data);
+  };
+
+  useEffect(() => {
+    getData("https://swapi.co/api/people")
+      .then(data => {
+        setState(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log(state);
+  });
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,6 +3,27 @@ import React from "react";
 const Character = ({ character }) => (
   <div className="Character">
     <h2>{character.name}</h2>
+    <p>
+      <span>Gender:</span> {character.gender}
+    </p>
+    <p>
+      <span>Birth year:</span> {character.birth_year}
+    </p>
+    <p>
+      <span>Height:</span> {character.height}
+    </p>
+    <p>
+      <span>Mass:</span> {character.mass}
+    </p>
+    <p>
+      <span>Eye color:</span> {character.eye_color}
+    </p>
+    <p>
+      <span>Skin color:</span> {character.skin_color}
+    </p>
+    <p>
+      <span>Hair color:</span> {character.hair_color}
+    </p>
   </div>
 );
 
