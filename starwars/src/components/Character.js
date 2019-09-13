0e873b4960f576f0f55575ddf9236ed89bacb722
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-13 09:15:58 +0100

    create StarWars and Character components

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Character = ({ character }) => (
+  <div className="Character">
+    <h2>{character.name}</h2>
+  </div>
+);
+
+export default Character;
---
+++ starwars/src/components/StarWars.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Character from "./Character";
+import "./StarWars.css";
+
+const StarWars = () => (
+  <div className="StarWars">
+    <Character />
+  </div>
+);
+
+export default StarWars;
