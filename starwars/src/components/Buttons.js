1b49672a7806eb7f35b881125fac4e8f6674521c
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-13 11:11:19 +0100

    disable button if no data

--- starwars/src/components/Buttons.js
+++ starwars/src/components/Buttons.js
@@ -3,8 +3,12 @@ import "./Buttons.css";
 
 const Buttons = ({ next, previous, action }) => (
   <div className="Buttons">
-    <button onClick={() => action(previous)}>Previous</button>
-    <button onClick={() => action(next)}>Next</button>
+    <button onClick={() => action(previous)} disabled={previous === null}>
+      Previous
+    </button>
+    <button onClick={() => action(next)} disabled={next === null}>
+      Next
+    </button>
   </div>
 );
 
