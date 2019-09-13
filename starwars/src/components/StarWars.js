33e0a745f3f4ff79568e2f35733bc483052ef56d
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-13 13:19:12 +0100

    convert StarWars styles to styled-components

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,13 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import Character from "./Character";
-import "./StarWars.css";
+
+const StarWarsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-gap: 1rem;
+  width: 100%;
+`;
 
 const StarWars = ({ characters }) => (
-  <div className="StarWars">
+  <StarWarsContainer>
     {characters.map(character => (
       <Character key={character.name} character={character} />
     ))}
-  </div>
+  </StarWarsContainer>
 );
 
 export default StarWars;
