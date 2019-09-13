396db3c1aeb88457b9aaa1b004df90ac300d7a02
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-13 14:20:00 +0100

    make project responsive

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -2,11 +2,22 @@ import React from "react";
 import styled from "styled-components";
 import Character from "./Character";
 
+const mobile = `(max-width: 480px)`;
+const tablet = `(max-width: 800px)`;
+
 const StarWarsContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   grid-gap: 1rem;
   width: 100%;
+
+  @media ${tablet} {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  @media ${mobile} {
+    grid-template-columns: 1fr;
+  }
 `;
 
 const StarWars = ({ characters }) => (
